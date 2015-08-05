dd8c326c636d0f9b7f92462d6a78dba9468af6af
Alex Kudryashov <akudryashov@newrelic.com>
2015-08-05 18:53:33 +0200

    Twisted official tutorial p4

--- twisted/finger/finger.py
+++ twisted/finger/finger.py
@@ -1,9 +1,29 @@
 from twisted.application import internet, service
 from twisted.internet import protocol, reactor, defer
 from twisted.protocols import basic
+from twisted.python import components
 from twisted.web import resource, server
-
+from zope.interface import Interface, implements
 import cgi
+
+
+class IFingerService(Interface):
+    def getUser(user):
+        """
+        REturn a deferred returning a string
+        """
+
+    def getUsers():
+        """
+        Return a deferred returning a list of strings
+        """
+
+
+class IFingerSetterService(Interface):
+    def setUser(user, status):
+        """
+        Set the user's status to something
+        """
 
 
 def catchError(err):
@@ -22,22 +42,77 @@ class FingerProtocol(basic.LineReceiver):
         d.addCallback(writeResponse)
 
 
-class UserStatus(resource.Resource):
-    def __init__(self, user, service):
-        resource.Resource.__init__(self)
-        self.user = user
+class IFingerFactory(Interface):
+    def getUser(user):
+        """
+        Return a deferred returning a string
+        """
+
+    def buildProtocol(addr):
+        """
+        Return a protocol returning a string
+        """
+
+
+class FingerFactoryFromService(protocol.ServerFactory):
+    implements(IFingerFactory)
+
+    protocol = FingerProtocol
+
+    def __init__(self, service):
         self.service = service
 
-    def render_GET(self, request):
-        d = self.service.getUser(self.user)
-        d.addCallback(cgi.escape)
-        d.addCallback(lambda m: '<h1>%s</h1>' % self.user + '<p>%s</p>' % m)
-        d.addCallback(request.write)
-        d.addCallback(lambda _: request.finish())
-        return server.NOT_DONE_YET
+    def getUser(self, user):
+        return self.service.getUser(user)
+
+
+components.registerAdapter(FingerFactoryFromService,
+                           IFingerService, IFingerFactory)
+
+
+class FingerSetterProtocol(basic.LineReceiver):
+    def connectionMade(self):
+        self.lines = []
+
+    def lineReceived(self, line):
+        self.lines.append(line)
+
+    def connectionLost(self, reason):
+        if len(self.lines) == 2:
+            self.factory.setUser(*self.lines)
+
+
+class IFingerSetterFactory(Interface):
+    def setUser(user, status):
+        """
+        Return a deferred returning a string
+        """
+
+    def buildProtocol(addr):
+        """
+        Return a protocol returning a string
+        """
+
+
+class FingerSetterFactoryFromService(protocol.ServerFactory):
+    implements(IFingerSetterFactory)
+
+    protocol = FingerSetterProtocol
+
+    def __init__(self, service):
+        self.service = service
+
+    def setUser(self, user, status):
+        self.service.setUser(user, status)
+
+
+components.registerAdapter(FingerSetterFactoryFromService,
+                           IFingerSetterService, IFingerSetterFactory)
 
 
 class UserStatusTree(resource.Resource):
+    implements(resource.IResource)
+
     def __init__(self, service):
         resource.Resource.__init__(self)
         self.service = service
@@ -61,7 +136,27 @@ class UserStatusTree(resource.Resource):
             return UserStatus(path, self.service)
 
 
+components.registerAdapter(UserStatusTree, IFingerService, resource.IResource)
+
+
+class UserStatus(resource.Resource):
+    def __init__(self, user, service):
+        resource.Resource.__init__(self)
+        self.user = user
+        self.service = service
+
+    def render_GET(self, request):
+        d = self.service.getUser(self.user)
+        d.addCallback(cgi.escape)
+        d.addCallback(lambda m: '<h1>%s</h1>' % self.user + '<p>%s</p>' % m)
+        d.addCallback(request.write)
+        d.addCallback(lambda _: request.finish())
+        return server.NOT_DONE_YET
+
+
 class FingerService(service.Service):
+    implements(IFingerService)
+
     def __init__(self, filename):
         self.users = {}
         self.filename = filename
@@ -78,16 +173,6 @@ class FingerService(service.Service):
     def getUsers(self):
         return defer.succeed(self.users.keys())
 
-    def getFingerFactory(self):
-        f = protocol.ServerFactory()
-        f.protocol = FingerProtocol
-        f.getUser = self.getUser
-        return f
-
-    def getResource(self):
-        r = UserStatusTree(self)
-        return r
-
     def startService(self):
         self._read()
         service.Service.startService(self)
@@ -96,13 +181,33 @@ class FingerService(service.Service):
         service.Service.stopService(self)
         self.call.cancel()
 
+
+class MemoryFingerService(service.Service):
+    implements([IFingerService, IFingerSetterService])
+
+    def __init__(self, **kwargs):
+        self.users = kwargs
+
+    def getUser(self, user):
+        return defer.succeed(self.users.get(user, "No such user"))
+
+    def getUsers(self):
+        return defer.succeed(self.users.keys())
+
+    def setUser(self, user, status):
+        self.users[user] = status
+
+
 application = service.Application('finger', uid=1, gid=1)
-f = FingerService('/Users/alex/study/python/twisted/finger/users')
+# f = FingerService('/Users/alex/study/python/twisted/finger/users')
+f = MemoryFingerService(alex='Twisted head')
+
 serviceCollection = service.IServiceCollection(application)
 f.setServiceParent(serviceCollection)
-internet.TCPServer(79, f.getFingerFactory()
+internet.TCPServer(79, IFingerFactory(f)).setServiceParent(serviceCollection)
+internet.TCPServer(8080, server.Site(resource.IResource(f))
                    ).setServiceParent(serviceCollection)
-fr = f.getResource()
-fr.putChild('', fr)
-internet.TCPServer(8080, server.Site(fr)
+internet.TCPServer(1079, IFingerFactory(f), interface='127.0.0.1'
+                   ).setServiceParent(serviceCollection)
+internet.TCPServer(1080, IFingerSetterFactory(f), interface='127.0.0.1'
                    ).setServiceParent(serviceCollection)
