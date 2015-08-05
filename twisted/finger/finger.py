272ccdcba6bc193e2301a27561f6921432fa5902
Alex Kudryashov <akudryashov@newrelic.com>
2015-08-05 13:05:16 +0200

    getter/setter factories

--- twisted/finger/finger.py
+++ twisted/finger/finger.py
@@ -1,5 +1,5 @@
 from twisted.application import internet, service
-from twisted.internet import reactor, protocol, defer
+from twisted.internet import protocol, defer
 from twisted.protocols import basic
 
 class FingerProtocol(basic.LineReceiver):
@@ -16,16 +16,46 @@ class FingerProtocol(basic.LineReceiver):
         d.addCallback(writeResponse)
 
 
-class FingerFactory(protocol.ServerFactory):
-    protocol = FingerProtocol
+class FingerSetterProtocol(basic.LineReceiver):
+    def connectionMade(self):
+        self.lines = []
 
+    def lineReceived(self, line):
+        self.lines.append(line)
+
+    def connectionLost(self, reason):
+        user = self.lines[0]
+        status = self.lines[1]
+        self.factory.setUser(user, status)
+
+
+class FingerService(service.Service):
     def __init__(self, **kwargs):
         self.users = kwargs
 
     def getUser(self, user):
         return defer.succeed(self.users.get(user, "No such user"))
 
+    def setUser(self, user, status):
+        self.users[user] = status
+
+    def getFingerFactory(self):
+        f = protocol.ServerFactory()
+        f.protocol = FingerProtocol
+        f.getUser = self.getUser
+        return f
+
+    def getFingerSetterFactory(self):
+        f = protocol.ServerFactory()
+        f.protocol = FingerSetterProtocol
+        f.setUser = self.setUser
+        return f
+
+f = FingerService(alex='o_O')
+
 application = service.Application('finger', uid=1, gid=1)
-factory = FingerFactory(alex='o_O')
-internet.TCPServer(79, factory).setServiceParent(
-    service.IServiceCollection(application))
+serviceCollection = service.IServiceCollection(application)
+internet.TCPServer(79, f.getFingerFactory()
+                   ).setServiceParent(serviceCollection)
+internet.TCPServer(1079, f.getFingerSetterFactory()
+                   ).setServiceParent(serviceCollection)
