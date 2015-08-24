bd2b96c1b9073352632171d8bc68767997b10b8e
Alex Kudryashov <alex.kudryashov@gmail.com>
2015-08-25 01:00:11 +0200

    To-do list that can handle exactly one item

--- django/the-goat-workshop/superlist/functional_tests.py
+++ django/the-goat-workshop/superlist/functional_tests.py
@@ -9,23 +9,28 @@ class HomePageTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def test_home_page_have_correct_title(self):
-        self.browser.get('http://localhost:8000')
-        self.assertIn('To-Do', self.browser.title)
+    #  def test_home_page_have_correct_title(self):
+        #  self.browser.get('http://localhost:8000')
+        #  self.assertIn('To-Do', self.browser.title)
 
-    def test_home_page_have_correct_header(self):
-        self.browser.get('http://localhost:8000')
-        header = self.browser.find_element_by_tag_name('h1')
-        self.assertIn('To-Do', header.text)
+    #  def test_home_page_have_correct_header(self):
+        #  self.browser.get('http://localhost:8000')
+        #  header = self.browser.find_element_by_tag_name('h1')
+        #  self.assertIn('To-Do', header.text)
 
-    def test_can_introduce_new_items(self):
+    def test_can_introduce_new_items_and_retrieve_it_later(self):
         self.browser.get('http://localhost:8000')
         input_box = self.browser.find_element_by_id('id_new_item')
-        self.assertIqual(input_box.get_attripute('placeholder'),
-                         'Enter a to-do item')
+        self.assertEqual(input_box.get_attribute('placeholder'),
+                         'Enter a to-do item: ')
 
         input_box.send_keys('Buy peacock feathers')
         input_box.send_keys("\n")
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('1: Buy peacock feathers',
+                      [row.text for row in rows])
 
 if __name__ == '__main__':
     unittest.main()
--- django/the-goat-workshop/superlist/lists/tests.py
+++ django/the-goat-workshop/superlist/lists/tests.py
@@ -7,9 +7,21 @@ from lists.views import home_page
 
 
 class HomePageViewTest(TestCase):
-    def test_home_page_resturns_correct_html(self):
+    def test_home_page_users_correct_template(self):
         request = HttpRequest()
         response = home_page(request)
         expected_template = render_to_string('home.html')
 
         self.assertEqual(expected_template, response.content)
+
+    def test_home_page_can_store_post_requests(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = 'new item'
+        response = home_page(request)
+
+        expected_content = render_to_string(
+            'home.html',
+            {'new_item_text': 'new item'})
+
+        self.assertEqual(response.content, expected_content)
